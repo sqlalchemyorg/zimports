6a43b62519586bf4c0b0b2010b5c26f25aca2458
Mike Bayer <mike_mp@zzzcomputing.com>
2019-10-25 18:54:59 -0400

    - bump to 0.2.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ readme = os.path.join(os.path.dirname(__file__), 'README.rst')
 
 setup(
     name='zimports',
-    version="0.1.3",
+    version="0.2.0",
     description="yet another import fixing tool",
     long_description=open(readme).read(),
     classifiers=[
