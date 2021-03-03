efee9605877a34e3e801eacc5e20805c561aedb2
Federico Caselli <cfederico87@gmail.com>
2021-03-03 22:22:32 +0100

    version to 0.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ readme = os.path.join(os.path.dirname(__file__), "README.rst")
 
 setup(
     name="zimports",
-    version="0.2.3",
+    version="0.3.0",
     description="yet another import fixing tool",
     long_description=open(readme).read(),
     classifiers=[
